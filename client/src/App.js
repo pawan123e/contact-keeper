7398c78f857416b321343ba3f9a4e49e955c17b7
unknown <pawanrealtek@gmail.com>
2020-07-01 19:04:04 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,81 @@
 import React from 'react';
+
 import './App.css';
+
 import Navbar from './components/layout/Navbar'
+
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 import Home from './components/pages/Home';
+
 import Register from './components/pages/Register';
+
 import Login from './components/pages/Login'
+
 import About from './components/pages/About'
+
 import ContactState from './context/contact/ContactState'
+
 import AlertState from './context/alert/AlertState'
+
 import AuthState from  './context/auth/AuthState'
+
 import Alert from './components/pages/Alert'
+
 import setAuthToken from './utils/setAuthToken'
+
 import PrivateRoute from './components/routing/PrivateRoute';
 
+
 if(localStorage.token) {
+ 
   setAuthToken(localStorage.token);
+
 }
+
 const App = () => {
-  return (
-    <div className="App">
-      <Router>
-        <AuthState>
-        <AlertState>
-        <ContactState>
-        <Navbar/>
-        <Alert/>
-        <Switch>
-          <PrivateRoute exact path='/' component={Home}/>
-          <Route path='/register' component={Register}/>
-          <Route path='/login' component={Login}/>
-          <Route path='/about' component={About}/>
-        </Switch>
-        </ContactState>
-        </AlertState>
-        </AuthState>
-      </Router>
+  
+return (
     
-    </div>
+<div className="App">
+      
+<Router>
+       
+ <AuthState>
+        
+<AlertState>
+        
+<ContactState>
+        
+<Navbar/>
+        
+<Alert/>
+        
+<Switch>
+          
+<PrivateRoute exact path='/' component={Home}/>
+          
+<Route path='/register' component={Register}/>
+          
+<Route path='/login' component={Login}/>
+          
+<Route path='/about' component={About}/>
+        
+</Switch>
+        
+</ContactState>
+        
+</AlertState>
+        
+</AuthState>
+      
+</Router>
+    
+    
+</div>
   );
+
 }
+
 
 export default App;
